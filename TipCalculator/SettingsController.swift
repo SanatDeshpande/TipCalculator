7455243ca5d167bda106382229b373e4f5663b64
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2017-01-05 18:28:01 -0500

    Tip caculator app

---
+++ TipCalculator/SettingsController.swift
@@ -0,0 +1,63 @@
+//
+//  SettingsController.swift
+//  TipCalculator
+//
+//  Created by Sanat Deshpande on 1/5/17.
+//  Copyright © 2017 Sanat Deshpande. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+
+    @IBOutlet weak var amount1: UITextField!
+    @IBOutlet weak var amount2: UITextField!
+    @IBOutlet weak var amount3: UITextField!
+    
+    var parentView: ViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        amount1.text = String(format: "%.0f", parentView.percentages[0] * 100)
+        amount2.text = String(format: "%.0f", parentView.percentages[1] * 100)
+        amount3.text = String(format: "%.0f", parentView.percentages[2] * 100)
+       
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeAmountOne(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let amt = (Double(amount1.text!)!)/100
+        defaults.setDouble(amt, forKey: "amountOne")
+        parentView.percentages[0] = amt
+        parentView.loadView()
+        parentView.viewDidLoad()
+    }
+   
+    @IBAction func changeAmountTwo(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let amt = (Double(amount2.text!)!)/100
+        defaults.setDouble(amt, forKey: "amountTwo")
+        parentView.percentages[1] = amt
+        parentView.loadView()
+        parentView.viewDidLoad()
+
+    }
+    
+    @IBAction func changeAmountThree(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let amt = (Double(amount3.text!)!)/100
+        defaults.setDouble(amt, forKey: "amountThree")
+        parentView.percentages[2] = amt
+        parentView.loadView()
+        parentView.viewDidLoad()
+    }
+    
+    
+}
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    @IBOutlet weak var tipSelection: UISegmentedControl!
+    var percentages = [0.15,0.2,0.25]
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "settingsSegue"){
+            let dest = segue.destinationViewController as! SettingsController
+            dest.parentView = self //as! ViewController
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 0..<3{
+            var s = String(format: "%.0f", percentages[i] * 100)
+            s += "%"
+            tipSelection.setTitle(s, forSegmentAtIndex: i)
+        }
+        
+        //let defaults = NSUserDefaults.standardUserDefaults()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +42,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(sender: AnyObject){
+        view.endEditing(true)
+    }
+   
+    @IBAction func calculateTip(sender: AnyObject) {
+        let bill = Double(billAmount.text!) ?? 0
+        
+        let tip = bill * percentages[tipSelection.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipAmount.text = String(format: "$%.2f", tip)
+        totalAmount.text = String(format: "$%.2f", total)
+        
+    }
+    
 }
 
